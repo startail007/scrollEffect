54c049a578681e36a3b23f182fa63644312102df
cai <flashmx29403229@gmail.com>
2020-09-23 16:21:37 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./index.css";
-let rangeValue = (val, min, max) => {
+const rangeValue = (val, min, max) => {
   return min + val * (max - min);
 };
-let mapVal = (val, min, max) => {
+const mapVal = (val, min, max) => {
   return (val - min) / (max - min);
 };
-let cropNumber = (val) => {
+const cropNumber = (val) => {
   if (val < 0) {
     return 0;
   }
@@ -35,7 +35,7 @@ let lagrangeInterpolation = (data, x) => {
   }
   return y;
 };*/
-let lagrangeInterpolation = (data, x) => {
+const lagrangeInterpolation = (data, x) => {
   let y = 0;
   const len = data.length;
   for (let i = 0; i < len; ++i) {
@@ -50,36 +50,37 @@ let lagrangeInterpolation = (data, x) => {
   }
   return y;
 }; //內插法
-
-let styleEase = (el, styleAttr, time, timeS, timeE, vals, calc, rule) => {
+const easeStep = (data, rate) => {
+  const vals = data.val;
+  const calc = data.calc;
+  const rule = data.rule;
   if (typeof vals[0] == "number") {
-    let val = lagrangeInterpolation(vals, cropNumber(mapVal(time, timeS, timeE)));
+    let val = lagrangeInterpolation(vals, rate);
     if (calc) {
       val = eval(calc.replace(/\$\{\d\}/g, val));
     }
     if (rule) {
-      el.style[styleAttr] = rule.replace(/\$\{\d\}/g, val);
+      return rule.replace(/\$\{\d\}/g, val);
     } else {
-      el.style[styleAttr] = val;
+      return val;
     }
   } else if (typeof vals[0] == "object") {
     if (vals[0] instanceof Array) {
-      let valList = [];
+      const valList = [];
       for (let i = 0; i < vals[0].length; i++) {
-        let temp = [];
+        const temp = [];
         for (let j = 0; j < vals.length; j++) {
           temp.push(vals[j][i]);
         }
-        valList[i] = lagrangeInterpolation(temp, cropNumber(mapVal(time, timeS, timeE)));
+        valList[i] = lagrangeInterpolation(temp, rate);
       }
 
-      let valList0 = [];
+      const valList0 = [];
       for (let i = 0; i < vals[0].length; i++) {
         if (calc && calc[i]) {
           valList0[i] = eval(
             calc[i].replace(/\$\{\d\}/g, (val) => {
-              let key = val.replace(/[\$\{\}]/g, "");
-              return valList[key];
+              return valList[val.replace(/[\$\{\}]/g, "")];
             })
           );
         } else {
@@ -87,160 +88,115 @@ let styleEase = (el, styleAttr, time, timeS, timeE, vals, calc, rule) => {
         }
       }
       if (rule) {
-        el.style[styleAttr] = rule.replace(/\$\{\d\}/g, (val) => {
-          let key = val.replace(/[\$\{\}]/g, "");
-          return valList0[key];
+        return rule.replace(/\$\{\d\}/g, (val) => {
+          return valList0[val.replace(/[\$\{\}]/g, "")];
         });
       }
     }
   }
 };
-let styleEaseGo = (selector) => {
-  return {
-    data: {},
-    setEl(selector) {
-      this.data.selector = selector;
-      this.data.el = document.body.querySelector(selector);
-      return this;
-    },
-    setTime(start, total) {
-      this.data.timeS = start;
-      this.data.timeE = start + total;
-      return this;
-    },
-    setTimeStoE(start, end) {
-      this.data.timeS = start;
-      this.data.timeE = end;
-      return this;
-    },
-    clone() {
-      let data = Object.assign({}, this.data); //JSON.parse(JSON.stringify(this.data));
-      //let data = JSON.parse(JSON.stringify(this.data));
-      data.el = this.data.el;
-      var copy = Object.assign({}, this);
-      copy.data = data;
-      return copy;
-    },
-    setParameter(styleAttr, vals, calc, rule) {
-      this.data.styleAttr = styleAttr;
-      this.data.parameters = vals;
-      this.data.calc = calc;
-      this.data.rule = rule;
-      //console.log(this.data.fun);
-      return this;
-    },
-    setData(data) {
-      this.data = data;
-      return this;
-    },
-    update(rate) {
-      //console.log(this.data);
-      styleEase(
-        this.data.el,
-        this.data.styleAttr,
-        rate,
-        this.data.timeS,
-        this.data.timeE,
-        this.data.parameters,
-        this.data.calc,
-        this.data.rule
+const styleEaseStep = (selector, style, rate) => {
+  const el = document.body.querySelector(selector);
+  for (let key in style) {
+    el.style[key] = easeStep(style[key], rate);
+  }
+};
+const sectionStyleEaseStep = (data, rate) => {
+  if (data) {
+    data.forEach((obj) => {
+      styleEaseStep(
+        obj.selector,
+        obj.style,
+        cropNumber(obj.time.duration == 0 ? 0 : mapVal(rate, obj.time.start, obj.time.start + obj.time.duration))
       );
-      return this;
-    },
-  }.setEl(selector);
+    });
+  }
 };
-//window.addEventListener("load", () => {
+const getSectionScrollData = (sectionList, scrollTop) => {
+  for (let i = 0; i < sectionList.length; i++) {
+    const el = sectionList[i];
+    const compStyles = window.getComputedStyle(el);
+    const h = el.clientHeight + parseFloat(compStyles.getPropertyValue("margin-bottom")) - window.innerHeight;
+    let rate = 0;
+    rate = h != 0 ? cropNumber((scrollTop - el.offsetTop) / h) : 0;
 
-let sectionList = document.getElementsByClassName("section");
-
-let styleEaseData = [];
-for (let i = 0; i < sectionList.length; i++) {
-  styleEaseData[i] = [[], [], []];
-}
-import list from "./data";
-/*import aaa from "./rgb_text.svg";
-console.log(aaa);*/
-let styleEaseGo01 = styleEaseGo();
-let keyMap = { start: 0, process: 1, end: 2 };
-for (let i = 0; i < sectionList.length; i++) {
-  if (list[i]) {
-    for (const name in list[i]) {
-      const el = list[i][name];
-      const index = keyMap[name];
-      el.forEach((obj) => {
-        styleEaseGo01.setEl(obj.selector).setTime(...obj.time);
-        for (let key in obj.style) {
-          styleEaseGo01.setParameter(key, obj.style[key].val, obj.style[key].calc, obj.style[key].rule);
-          styleEaseData[i][index].push(Object.assign({}, styleEaseGo01.data));
-        }
-      });
+    if (rate >= 0 && rate < 1) {
+      return { type: "run", index: i, rate: rate };
+    }
+    rate = cropNumber((scrollTop - el.offsetTop - h) / window.innerHeight);
+    if (rate >= 0 && rate < 1) {
+      return { type: "ease", index: i, rate: rate };
     }
   }
-}
-//資料轉換
+};
 
-let rateList = [];
-let BoolList = [];
-for (let i = 0; i < sectionList.length * 2; i++) {
-  rateList[i] = 0;
-}
-for (let i = 0; i < sectionList.length; i++) {
-  BoolList[i] = [false, false, false];
-}
+const sectionList = document.getElementsByClassName("section");
 
-let scroll = () => {
-  let scrollTop = document.doctype ? document.documentElement.scrollTop : document.body.scrollTop;
-  for (let i = 0; i < sectionList.length; i++) {
-    let el = sectionList[i];
-    let compStyles = window.getComputedStyle(el);
-    let h = el.clientHeight + parseFloat(compStyles.getPropertyValue("margin-bottom")) - window.innerHeight;
+import list from "./data";
 
-    rateList[i * 2] = h != 0 ? cropNumber((scrollTop - el.offsetTop) / h) : 0;
-    rateList[i * 2 + 1] = cropNumber((scrollTop - el.offsetTop - h) / window.innerHeight);
+let rate = 0;
+let cType = "run";
+let cIndex = 0;
+
+const init = () => {
+  const scrollTop = document.doctype ? document.documentElement.scrollTop : document.body.scrollTop;
+  let sectionScrollData = getSectionScrollData(sectionList, scrollTop);
+  rate = sectionScrollData.rate;
+  cType = sectionScrollData.type;
+  cIndex = sectionScrollData.index;
+  for (let i = 0; i < cIndex + 1; i++) {
+    if (i < cIndex) {
+      sectionStyleEaseStep(list[i]["start"], 1);
+      sectionStyleEaseStep(list[i]["process"], 1);
+      sectionStyleEaseStep(list[i]["end"], 1);
+    } else {
+      sectionStyleEaseStep(list[i]["start"], 1);
+      if (cType == "ease") {
+        sectionStyleEaseStep(list[i]["process"], 1);
+      }
+    }
   }
-
-  console.log(rateList);
-  let runFun = (rates, bools, fun) => {
-    rates.forEach((val, index, array) => {
-      if (index - 1 < 0 || array[index - 1] >= 1) {
-        if (!bools[index] && val >= 0) {
-          if (val >= 1) {
-            bools[index] = true;
-          }
-          if (fun) {
-            fun.call(this, val, bools[index], index);
-          }
-        } else if (bools[index] && val < 1) {
-          bools[index] = false;
+  //將所有元素設定好
+};
+const scroll = () => {
+  const scrollTop = document.doctype ? document.documentElement.scrollTop : document.body.scrollTop;
+  let sectionScrollData = getSectionScrollData(sectionList, scrollTop);
+  rate = sectionScrollData.rate;
+  if (cType != sectionScrollData.type || cIndex != sectionScrollData.index) {
+    if (cType == "run") {
+      sectionStyleEaseStep(list[cIndex]["process"], 1);
+    } else if (cType == "ease") {
+      if (cIndex == sectionScrollData.index) {
+        sectionStyleEaseStep(list[cIndex]["end"], 0);
+        if (cIndex + 1 < list.length) {
+          sectionStyleEaseStep(list[cIndex + 1]["start"], 0);
+        }
+      } else {
+        sectionStyleEaseStep(list[cIndex]["end"], 1);
+        if (cIndex + 1 < list.length) {
+          sectionStyleEaseStep(list[cIndex + 1]["start"], 1);
         }
       }
-    });
-  };
-
+    }
+    cType = sectionScrollData.type;
+    cIndex = sectionScrollData.index;
+  } //切換狀態與頁面時 把元素歸位準確
   for (let i = 0; i < sectionList.length; i++) {
-    runFun(
-      [i * 2 - 1 >= 0 ? rateList[i * 2 - 1] : 1, rateList[i * 2], rateList[i * 2 + 1]],
-      BoolList[i],
-      (rate, bool, index) => {
-        let styleEaseGo01 = styleEaseGo();
-        styleEaseData[i][index].forEach((data) => {
-          styleEaseGo01.setData(data);
-          styleEaseGo01.update(rate);
-        });
-      }
-    );
-  }
-  let count = 2;
-  for (let i = 0; i < sectionList.length; i++) {
-    let bool = BoolList[i].reduce((p, c) => p + (c ? 1 : 0), 0) == 3;
-    if (!bool && count > 0) {
-      count--;
+    if (i >= cIndex && i <= cIndex + 1) {
       sectionList[i].style.visibility = "";
     } else {
       sectionList[i].style.visibility = "hidden";
     }
-  }
+  } //隱藏頁面
+  if (cType == "run") {
+    sectionStyleEaseStep(list[cIndex]["process"], rate);
+  } else if (cType == "ease") {
+    sectionStyleEaseStep(list[cIndex]["end"], rate);
+    if (cIndex + 1 < list.length) {
+      sectionStyleEaseStep(list[cIndex + 1]["start"], rate);
+    }
+  } //執行效果
 };
 window.addEventListener("scroll", scroll);
+init();
 scroll();
-//});
