e5191f21b6812d576e2e7b312750662bef482042
cai <flashmx29403229@gmail.com>
2020-09-27 23:35:33 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -75,9 +75,7 @@ const easeStep = (el, vals, rule, unit = "", rate) => {
       let val = lagrangeInterpolation(vals, rate);
       if (rule) {
         val = runFunction(
-          indexReplace(rule, () => {
-            return val + unit;
-          }),
+          indexReplace(rule, () => val),
           el,
           listDataFun
         );
@@ -164,44 +162,78 @@ const splitGroup = (val, changeNum) => {
   }
 };
 const splitTime = (val, changeNum) => {
-  return val.split(/\~/g).map((val_0) => {
+  const list = [];
+  val.replace(/\~|\_/g, (val) => {
+    list.push(val == "_");
+  });
+  let data = val.split(/\~|\_/g).map((val_0) => {
     return changeNum ? changeNum(val_0) : val_0;
   });
+  let temp;
+  return data.map((val, index, array) => {
+    if (typeof val == "object" && val instanceof Array) {
+      if (index != 0 && list[index - 1]) {
+        temp = temp.map((val_0, index) => {
+          return val_0 + val[index];
+        });
+      } else {
+        temp = Object.assign([], val);
+      }
+      return temp;
+    } else {
+      if (index != 0 && list[index - 1]) {
+        temp += val;
+      } else {
+        temp = val;
+      }
+      return temp;
+    }
+  });
 };
+
 const indexReplace = (val, changeNum) => {
   return val.replace(/\$\{\d\}/g, (val) => {
     return changeNum ? changeNum(val) : val;
   });
 };
 const runFunction = (val, el, funs) => {
-  if (/\$f/g.test(val)) {
-    let vals = val.split(/\$f/g);
-    vals = vals.filter((val) => val !== "").map((val) => "$f" + val);
+  const re_f = /\$f/g;
+  if (re_f.test(val)) {
+    let s_temp = [];
+    if (/\'/g.test(val)) {
+      const list = val.split(/\'/g);
+      s_temp = list.filter((val, index) => index % 2 == 1);
+      val = list.map((val, index) => (index % 2 == 1 ? "$s" + Math.floor(index / 2) : val)).join("");
+    } //先分割字串
 
-    return vals
+    val = val
+      .split(re_f)
+      .filter((val) => val !== "")
       .map((val) => {
-        return val.replace(/\$f\{.*\}/g, (ss) => {
-          const s = ss.replace(/^\$f\{(.*)\}$/g, "$1");
-          const s0 = s.replace(/^(.+)\(.*\)$/g, "$1");
-
-          if (funs[s0]) {
-            let s1_data = [];
-            if (/^.+\((.+)\)$/g.test(s)) {
-              s1_data = s
-                .replace(/^.+\((.+)\)$/g, "$1")
-                .split(/\,/g)
-                .map(eval);
+        return val.replace(/^\{(\w+)\((.*)\)\}/g, (val, name, parameter) => {
+          const p = parameter.split(/\,/g).map((val) => {
+            if (s_temp.length > 0) {
+              const re_s = /\$f/g;
+              if (re_s.test(val)) {
+                val = "'" + s_temp[val.replace(re_s, "")] + "'";
+              }
             }
-            return funs[s0].apply(this, [el, ...s1_data]);
+            try {
+              return eval(val);
+            } catch (error) {
+              return val;
+            }
+          });
+          if (funs[name]) {
+            return funs[name].apply(this, [el, ...p]);
           } else {
-            return ss;
+            return val;
           }
         });
       })
       .join("");
-  } else {
-    return val;
   }
+  return val;
 };
 
 const splitStyle = (val, rate) => {
@@ -266,17 +298,13 @@ const listDataFun = {
     return el.getTotalLength();
   },
   wave: (el, val) => {
-    return 20 * Math.sin(val * 2 * Math.PI);
-  },
-  wave01: (el, val) => {
-    //console.log(el, val);
     const w = window.innerWidth;
     const h = 70;
     let s = "";
     const n = 20;
     const r = 30 * lagrangeInterpolation([0.5, 0.75, 0.9, 1, 0], val);
     //const r = 30;
-    const m = w / 800;
+    const m = w / (800 - 400 * val);
     for (let i = 0; i <= n; i++) {
       const rate = i / n;
       const angle = (6 * val + m * rate) * 2 * Math.PI;
@@ -286,6 +314,14 @@ const listDataFun = {
     }
     return `M ${w} ${h} L 0 ${h}` + s;
   },
+  wave_texture: (el, x, y, rate) => {
+    x += 10 * Math.sin(2.4 * rate * 2 * Math.PI);
+    y += 15 * Math.cos(4 * rate * 2 * Math.PI);
+    return x + "," + y;
+  },
+  bubble: (el, val, rate) => {
+    return val + 1 * Math.sin(2 * rate * 2 * Math.PI);
+  },
 };
 let cRate = 0;
 const listData = [];
@@ -293,79 +329,85 @@ list.forEach((obj) => {
   const elList = document.body.querySelectorAll(obj.selector);
   [...elList].forEach((el, index, array) => {
     const elRate = index / (array.length - 1);
-    let time = timeDecomposition(obj["time"], elRate);
-    let timeProportion = timeDecomposition(obj["timeProportion"], elRate);
+    let time = timeDecomposition(obj.time, elRate);
+    let timeProportion = timeDecomposition(obj.timeProportion, elRate);
     if (timeProportion) {
       time.start = rangeValue(time.start, timeProportion.start, timeProportion.end);
       time.end = rangeValue(time.end, timeProportion.start, timeProportion.end);
     }
+    const count = obj.count == undefined ? 1 : obj.count;
+    const duration = (time.end - time.start) / count;
+    for (let i = 0; i < count; i++) {
+      const item = {
+        el: el,
+        start: time.start,
+        end: time.end,
+        data: [],
+      };
+      if (count > 1) {
+        item.start = time.start + duration * i;
+        item.end = time.start + duration * (i + 1);
+      }
+      for (let styleKey in obj) {
+        if (styleKey != "selector" && styleKey != "time" && styleKey != "timeProportion") {
+          let unit;
+          let vals = obj[styleKey];
+          let falseVals;
+          let switchBool = false;
 
-    const item = {
-      el: el,
-      start: time.start,
-      end: time.end,
-      data: [],
-    };
-    //const style = [];
-    for (let styleKey in obj) {
-      if (styleKey != "selector" && styleKey != "time" && styleKey != "timeProportion") {
-        let unit;
-        let vals = obj[styleKey];
-        let falseVals;
-        let switchBool = false;
-
-        if (typeof vals == "object") {
-          vals = vals.val;
-        }
-        if (vals != undefined) {
-          if (typeof vals == "string") {
-            const data = vals.split(/\_/g);
-            vals = data[0];
-            unit = data[1] || unit;
-            switchBool = /\&/g.test(vals);
-
-            if (switchBool) {
-              vals = vals.split(/\&/g);
-
-              vals[1] = runFunction(vals[1], el, listDataFun);
-              falseVals = splitStyle(vals[1], elRate);
-
-              vals[0] = runFunction(vals[0], el, listDataFun);
-              vals = splitStyle(vals[0], elRate);
-            } else {
-              vals = runFunction(vals, el, listDataFun);
-              vals = splitStyle(vals, elRate);
-            }
-          } else if (typeof vals == "number") {
-            vals = [vals];
+          if (typeof vals == "object") {
+            vals = vals.val;
           }
-          const attr = /^attr\_/g.test(styleKey);
-          item.data.push({
-            name: attr ? styleKey.replace(/^attr\_/g, "") : styleKey,
-            val: vals,
-            rule: obj[styleKey].rule,
-            unit: unit,
-            falseVal: falseVals,
-            attr: attr,
-          });
+          if (vals != undefined) {
+            if (typeof vals == "string") {
+              const data = vals.split(/\?/g);
+              vals = data[0];
+              unit = data[1] || unit;
+              switchBool = /\!/g.test(vals);
+
+              if (switchBool) {
+                vals = vals.split(/\!/g);
+
+                vals[1] = runFunction(vals[1], el, listDataFun);
+                falseVals = splitStyle(vals[1], elRate);
+
+                vals[0] = runFunction(vals[0], el, listDataFun);
+                vals = splitStyle(vals[0], elRate);
+              } else {
+                vals = runFunction(vals, el, listDataFun);
+                vals = splitStyle(vals, elRate);
+              }
+            } else if (typeof vals == "number") {
+              vals = [vals];
+            }
+            const attr = /^attr\_/g.test(styleKey);
+            item.data.push({
+              name: attr ? styleKey.replace(/^attr\_/g, "") : styleKey,
+              val: vals,
+              rule: obj[styleKey].rule,
+              unit: unit,
+              falseVal: falseVals,
+              attr: attr,
+            });
+          }
         }
       }
+      listData.push(item);
     }
-    listData.push(item);
   });
 }); //資料轉換
 const listDataBool = listData.map(() => false);
 console.log(listData, listDataBool);
-//time 可使用符號 | -> _ ~ 切割順序 (~ _) -> |
+//time 可使用符號 順序 [~ or _] -> |
 //----------------------
 //0.1|0.3 間隔亂數 0.1到0.3之間亂數
 //0.1->0.3 元件接續變化 假設3個元件 分配到0.1秒 0.2秒 0.3秒
 //0.1_0.3 時間範圍 開始0.1秒 持續0.3秒
 //0.1~0.3 時間範圍 開始0.1秒 結束0.3秒
 
-//style 可使用符號 & ~ , -> | 切割順序 & ~ , -> |
+//style 可使用符號 順序 ? [~ or _] , -> |
 //----------------------
-//123&456 範圍開關 在進入時間範圍時 顯示123 反之456
+//123?456 範圍開關 在進入時間範圍時 顯示123 反之456
 //0.1~0.3 時間接續漸變 假設時間0~1秒變化對應0.1~0.3
 //10,5 分組 ${0} = 10 ${1} = 5
 //0.1->0.3 元件接續變化 假設3個元件 分配到0.1 0.2 0.3
@@ -390,15 +432,19 @@ const scroll = () => {
       if (cRate < obj.start) {
         listDataBool[index] = true;
         dataEaseStep(obj.el, obj.data, 0, false);
-        //attrEaseStep(obj.el, obj.attr, 0, false);
       } else if (cRate > obj.end) {
         listDataBool[index] = true;
         dataEaseStep(obj.el, obj.data, 1, false);
-        //attrEaseStep(obj.el, obj.attr, 1, false);
       } else {
         const rate0 = obj.end - obj.start <= 0 ? 0 : mapVal(cRate, obj.start, obj.end);
+        /*if (
+          obj.el == document.getElementById("section03_bubble01") ||
+          obj.el == document.getElementById("section03_bubble02") ||
+          obj.el == document.getElementById("section03_bubble03")
+        ) {
+          console.log(obj.el, rate0, obj.start, obj.end);
+        }*/
         dataEaseStep(obj.el, obj.data, rate0, true);
-        //styleEaseStep(obj.el, obj.attr, rate0, true);
       }
     } else {
       if (cRate >= obj.start && cRate <= obj.end) {
@@ -409,3 +455,7 @@ const scroll = () => {
 };
 window.addEventListener("scroll", scroll);
 init();
+
+/*let star01 = document.getElementById("star01");
+star01.setAttribute("patternTransform", "translate(0,600)");
+console.log(star01.patternTransform);*/
